5aa9b98f2db90cb02d4fc96f0a7a042a582faf4d
deepak <deepakdpyqaz@gmail.com>
2021-12-30 19:23:44 +0530

    Updates

--- src/components/FullLoader.jsx
+++
@@ -1,9 +0,0 @@
-import CircularProgress from "@mui/material/CircularProgress";
-const FullLoader = (props) => {
-    return (
-        <div style={{}}>
-            <CircularProgress color="primary" />
-        </div>
-    )
-}
-export default FullLoader;
